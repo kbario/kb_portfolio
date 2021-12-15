b618beb7a1f481bf554b64b12a250af377a9ebba
kbario <kylebario1@gmail.com>
2021-12-15 23:10:34 +0800

    bg img added and phone functionality

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function menu() {
+    const ns  = [1,2,3,4]
+    for (i in ns){
+        document.getElementById(`burg${ns[i]}`).classList.toggle('twist')
+    }
+    document.querySelector("nav").classList.toggle('twist')
+}
